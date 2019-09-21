10338dd52224406351bc599b50fbca6d46bd9dbd
Ngheizit <ngheizitwu@163.com>
2019-09-21 10:30:58 +0800

    redo and Create an AE_seetable~

---
+++ ArcGISEngine/AE_显示要素属性表格/ShowFeatureTable/FormMain.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ShowFeatureTable
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.axTOCControl1 = new ESRI.ArcGIS.Controls.AxTOCControl();
+            this.listView_table = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(240, 193);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 0;
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Location = new System.Drawing.Point(-1, -1);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(852, 554);
+            this.axMapControl1.TabIndex = 1;
+            this.axMapControl1.OnMouseDown += new ESRI.ArcGIS.Controls.IMapControlEvents2_Ax_OnMouseDownEventHandler(this.axMapControl1_OnMouseDown);
+            // 
+            // axTOCControl1
+            // 
+            this.axTOCControl1.Location = new System.Drawing.Point(850, -1);
+            this.axTOCControl1.Name = "axTOCControl1";
+            this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
+            this.axTOCControl1.Size = new System.Drawing.Size(234, 554);
+            this.axTOCControl1.TabIndex = 2;
+            this.axTOCControl1.OnMouseDown += new ESRI.ArcGIS.Controls.ITOCControlEvents_Ax_OnMouseDownEventHandler(this.axTOCControl1_OnMouseDown);
+            // 
+            // listView_table
+            // 
+            this.listView_table.Location = new System.Drawing.Point(-1, 355);
+            this.listView_table.Name = "listView_table";
+            this.listView_table.Size = new System.Drawing.Size(852, 198);
+            this.listView_table.TabIndex = 3;
+            this.listView_table.UseCompatibleStateImageBehavior = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 552);
+            this.Controls.Add(this.listView_table);
+            this.Controls.Add(this.axTOCControl1);
+            this.Controls.Add(this.axMapControl1);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private ESRI.ArcGIS.Controls.AxTOCControl axTOCControl1;
+        private System.Windows.Forms.ListView listView_table;
+    }
+}
+
---
+++ ArcGISEngine/AE_显示要素属性表格/ShowFeatureTable/FormMain.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+
+namespace ShowFeatureTable
+{
+    public partial class FormMain : Form
+    {
+        #region 属性变量（类域）
+
+        // 工作空间 "......\ShowFeatureTable\bin\Debug" 相对路径
+        public string DEBUG_DIR = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+
+        private IMapControl2 pMapC2;
+        #endregion
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        #region 自定义方法
+        /// <summary>
+        /// 根据名称添加矢量数据
+        /// </summary>
+        /// <param name="filename">当前工作目录下的.shp文件名，带后缀</param>
+        /// <param name="index">添加到的图层位置索引，从0开始，默认为0</param>
+        private void gis_AddShapefileByPath(string filename, int index = 0)
+        {
+            if (this.pMapC2 != null)
+            {
+                this.pMapC2.AddShapeFile(this.DEBUG_DIR, filename);
+                pMapC2.MoveLayerTo(0, index);
+            }
+        }
+        /// <summary>
+        /// 添加表头字段
+        /// </summary>
+        /// <param name="name">表头字段名</param>
+        /// <param name="width">表头字段显示长度，默认为100</param>
+        private void gis_CreateTableHeader(string name, int width = 100)
+        {
+            ColumnHeader colhdr = new ColumnHeader();
+            colhdr.Text = name;
+            colhdr.Width = width;
+            listView_table.Columns.Add(colhdr);
+        }
+        #endregion
+
+
+
+        #region 窗体加载时
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region 属性初始化
+            this.pMapC2 = axMapControl1.GetOcx() as IMapControl2;
+            axTOCControl1.SetBuddyControl(axMapControl1);
+            #region listView控件初始属性
+            listView_table.Visible = false;
+            listView_table.HeaderStyle = ColumnHeaderStyle.Clickable;
+            listView_table.View = View.Details; 
+            #endregion
+            #endregion
+
+            #region 初始图层加载
+            string[] shapefiles = new string[] { "Continents.shp", "Lakes.shp", "Rivers.shp", "Cities.shp" };
+            foreach (string shp in shapefiles)
+            {
+                this.gis_AddShapefileByPath(shp);
+            } 
+            #endregion
+
+        } 
+        #endregion
+
+        #region MapControl控件鼠标点击事件
+        private void axMapControl1_OnMouseDown(object sender, IMapControlEvents2_OnMouseDownEvent e)
+        {
+            #region 中键点击：地图移动
+            if (e.button == 4)
+            {
+                this.pMapC2.MousePointer = esriControlsMousePointer.esriPointerPanning;
+                this.pMapC2.Pan();
+                this.pMapC2.MousePointer = esriControlsMousePointer.esriPointerArrow;
+            }
+            #endregion
+        } 
+        #endregion
+
+        private void axTOCControl1_OnMouseDown(object sender, ITOCControlEvents_OnMouseDownEvent e)
+        {
+            IBasicMap map = new MapClass();
+            ILayer layer = new FeatureLayerClass(); // Carto
+            object other = new object();
+            object index = new object();
+            esriTOCControlItem item = new esriTOCControlItem();
+            axTOCControl1.HitTest(e.x, e.y, ref item, ref map, ref layer, ref other, ref index);
+
+
+
+            if (layer != null)
+            {
+                #region 可视化表格
+                if (!listView_table.Visible)
+                {
+                    axMapControl1.Height -= listView_table.Height;
+                    listView_table.Visible = true;
+                } 
+                #endregion
+
+                #region 清空可能已有的表格字段和属性列
+                listView_table.Items.Clear();
+                listView_table.Columns.Clear(); 
+                #endregion
+
+                // 获取当前点击图层的信息
+                IFeatureLayer pFeatureLayer = layer as IFeatureLayer;
+                IFields pFields = pFeatureLayer.FeatureClass.Fields;
+
+                // 循环添加表头信息
+                for (int i = 0; i < pFields.FieldCount; i++)
+                {
+                    this.gis_CreateTableHeader(pFields.Field[i].Name);
+                }
+
+                // 循环添加要素属性信息
+                IFeatureCursor pFeatureCursor = pFeatureLayer.FeatureClass.Search(null, false);
+                IFeature pFeature = pFeatureCursor.NextFeature();
+                while (pFeature != null)
+                {
+                    string[] fldVaue = new string[pFields.FieldCount];
+
+                    // 顺换添加要素中的每个字段信息
+                    for (int i = 0; i < pFields.FieldCount; i++)
+                    {
+                        string fldName = pFields.get_Field(i).Name;
+                        if (fldName == pFeatureLayer.FeatureClass.ShapeFieldName)
+                        {
+                            // 去除 "esrigeometry"前缀
+                            string type = Convert.ToString(pFeature.Shape.GeometryType).Substring(12);
+                            fldVaue[i] = type;
+                        }
+                        else
+                            fldVaue[i] = Convert.ToString(pFeature.get_Value(i)); // 属性信息
+                    }
+
+                    ListViewItem lstVwItem = new ListViewItem(fldVaue);
+                    listView_table.Items.Add(lstVwItem);
+
+                    pFeature = pFeatureCursor.NextFeature(); // 迭代
+                }
+            }
+        }
+    }
+}
